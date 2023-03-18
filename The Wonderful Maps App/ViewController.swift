736a6f0a02ba3dfdea4d3ced77813984275b56fa
Gabriel Medina <rha7.com@gmail.com>
2023-03-17 23:14:42 -0600

    Initial Commit

---
+++ The Wonderful Maps App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  The Wonderful Maps App
+//
+//  Created by Juan Gabriel Medina Marquez on 17/03/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
